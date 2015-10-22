6511480af5254fc49f50b9bae88d34d5bd54e4af
wendal <wendal1985@gmail.com>
2015-10-23 06:04:44 +0800

    add: 支持网站新功能: 推送到手机

--- app/src/main/java/net/wendal/nutzbook/util/JpushReceiver.java
+++ app/src/main/java/net/wendal/nutzbook/util/JpushReceiver.java
@@ -18,11 +18,26 @@ public class JpushReceiver extends android.content.BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.i("jpush", intent.toString());
         Bundle bundle = intent.getExtras();
+        String extras = bundle == null ? "{}" : bundle.getString(JPushInterface.EXTRA_EXTRA);
+        if (extras == null)
+            extras = "{}";
+        Log.i("jpush", extras);
         if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
-            String type = bundle.getString(JPushInterface.EXTRA_EXTRA);
-            Log.i("jpush", type);
             try {
-                TopicActivity.open(context, new JSONObject(type).getString("topic_id"));
+                String topic_id = new JSONObject(extras).getString("topic_id");
+                if (topic_id != null)
+                    TopicActivity.open(context, topic_id);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
+            try {
+                JSONObject json = new JSONObject(extras);
+                String topic_id = json.getString("topic_id");
+                String action = json.getString("action");
+                if (topic_id != null && "open_topic".equals(action))
+                    TopicActivity.open(context, topic_id);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
